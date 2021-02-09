5680535b5ac2331ece258317e1a01be6ebb2cb3e
nganifaith <nganifaith@gmail.com>
2021-02-09 09:11:07 +0100

    fix linter error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,20 @@
 # Build a method #bubble_sort that takes an array and returns a sorted array.
-# It must use the bubble sort methodology (using #sort would be pretty pointless, wouldn’t it?).
+# It must use the bubble sort methodology.
 
+array = [11, 10, 12, 1, 100, 14, 0, 99, 2]
 
-array = [11,10,12,1,100,14,0,99,2]
-
-def bubble_sort (array)
+def bubble_sort(array)
   array.length().times do 
-    array.each_with_index do |v,i|
-      if array[i+1]
-        if v > array[i+1]
-          array[i] , array[i+1] = array[i+1] , array[i]
+    array.each_with_index do |v, i|
+      if array[i + 1]
+        if v > array[i + 1]
+          array[i], array[i+1] = array[i+1], array[i]
+        next 
         end
       end
     end
   end
-  return array
+  array
 end
 
 puts bubble_sort(array).to_a
