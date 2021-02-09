f1f0ef26d7f7f209e3482a004233f873e18a54d3
nganifaith <nganifaith@gmail.com>
2021-02-09 09:14:10 +0100

    fix linter errors on space

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,12 +4,12 @@
 array = [11, 10, 12, 1, 100, 14, 0, 99, 2]
 
 def bubble_sort(array)
-  array.length().times do 
+  array.length.times do
     array.each_with_index do |v, i|
       if array[i + 1]
         if v > array[i + 1]
-          array[i], array[i+1] = array[i+1], array[i]
-        next 
+          array[i], array[i + 1] = array[i + 1], array[i]
+          next
         end
       end
     end
