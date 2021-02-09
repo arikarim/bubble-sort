f0c412a30c48c5c0c453714cd464d297b2d5ac5e
ari karim <arikarim146@gmail.com>
2021-02-09 11:43:13 +0300

    added bubble_sort_by

---
+++ bubble_sort_by.rb
@@ -0,0 +1,15 @@
+#Now lets sort words 
+def bubble_sort_by array
+  (array.length).times do
+    (array.length-1).times do |i|
+      if yield(array[i], array[i + 1]) > 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+      end
+    end
+  end
+  array
+end
+sorted_array= bubble_sort_by(["norway", "hi", "hello", "hey"]) do |left,right|
+  left.length - right.length
+end
+puts sorted_array
