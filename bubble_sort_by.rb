8906cca760ccb794cd86c18be2b1cfa2b3616955
nganifaith <nganifaith@gmail.com>
2021-02-09 09:59:19 +0100

    replace > with postive?

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,7 +2,7 @@
 def bubble_sort_by(array)
   array.length.times do
     (array.length - 1).times do |i|
-      if yield(array[i], array[i + 1]) > 0
+      if yield(array[i], array[i + 1]).positive?
         array[i], array[i + 1] = array[i + 1], array[i]
       end
     end
